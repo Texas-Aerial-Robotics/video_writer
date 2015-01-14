9ae6a5d90d2899e3937b335fcba8326370ca7e73
Santosh Thoduka <santosh.thoduka@gmail.com>
2015-01-14 15:51:07 +0100

    add copyright notice

--- ros/src/video_writer_node.cpp
+++ ros/src/video_writer_node.cpp
@@ -1,3 +1,11 @@
+/* video_writer_node.cpp
+ *
+ * Copyright (C) 2014 Santosh Thoduka
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 #include <video_writer/video_writer_node.h>
 #include <sensor_msgs/image_encodings.h>
 #include <cv_bridge/cv_bridge.h>
